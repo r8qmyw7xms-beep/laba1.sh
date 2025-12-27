ec5bfdaddcba3b15726bb549b6b77de122b7b73e
Egormai8 <r8qmyw7xms@privaterelay.appleid.com>
2025-12-27 11:07:13 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+mkdir labwork
+cd labwork
+touch file.txt
+echo "apple" >> file.txt
+echo "banana" >> file.txt
+echo "APPLE" >> file.txt
+echo "cherry" >>file.txt
+cat file.txt | wc -l
+grep -ni "apple" file.txt
+cp -v file.txt file2.txt
+file file2.txt
+pwd
+mv -v file.txt last.txt
+rm -i file2.txt
+cat last.txt
 
